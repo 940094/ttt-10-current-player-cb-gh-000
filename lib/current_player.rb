057710b2d7a304d2af6c0192795b08f32be2b177
Justin Shaw <easyworkemail@gmail.com>
2019-04-28 13:30:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,15 @@
-def turn_count()
-  
+def turn_count(board)
+  # Assumes player who goes first is 'X'
+  # When X goes, there is 1 occupied space so it's O's turn. When ) goes, there are 2 occupied spaces, so it's X's turn
+  # iterate through each board and keep count of Xs or Os
+  count = 0
+  board.each do |cell|
+    if cell == 'X' || cell == 'O'
+      count += 1
+    end
+  end
+  print count
+  return turns_played
 end
 
 def current_player()
